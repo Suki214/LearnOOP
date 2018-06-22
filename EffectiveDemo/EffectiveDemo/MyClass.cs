11503971ed7775db3d5bae8dce393666fb31b56a
Suki214 <38032046+Suki214@users.noreply.github.com>
2018-06-22 15:06:50 +0800

    Add files via upload

---
+++ EffectiveDemo/EffectiveDemo/MyClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EffectiveDemo
+{
+    class MyClass : IMsg
+    {
+        public void Message()
+        {
+            ShowMessage();
+        }
+        public virtual void ShowMessage()
+        {
+            Console.WriteLine("MyClass");
+        }
+    }
+}
---
+++ EffectiveDemo/EffectiveDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EffectiveDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDerivedClass d = new MyDerivedClass();
+            d.Message();
+            IMsg m = d as IMsg;
+            m.Message();
+            MyClass myClass = d;
+            myClass.Message();
+            Console.ReadKey();
+            //Result:
+            //MyDerivedClass
+            //MyDerivedClass
+            //MyDerivedClass
+        }
+    }
+}
