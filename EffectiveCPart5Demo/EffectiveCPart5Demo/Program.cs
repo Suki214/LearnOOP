9326f352cb5c3e49df242e56c0b2e93b62d18e71
suki.xu@infor.com <suki.xu@infor.com>
2018-06-29 16:10:50 +0800

    add effective C# part5 demo

---
+++ EffectiveCPart5Demo/EffectiveCPart5Demo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EffectiveCPart5Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //add for test
+        }
+    }
+}
