cedbbc2e449a40d50880837e6939fca4d0e22f04
suki.xu@infor.com <suki.xu@infor.com>
2018-06-29 15:47:08 +0800

    add

---
+++ ITHelpDeskDemoApp-Part-01/ITHelpDeskDemoApp/ITHelpDeskDemoApp/ITHelpDeskBootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Practices.Prism.UnityExtensions;
+using Microsoft.Practices.Unity;
+using System.Windows;
+
+namespace ITHelpDeskDemoApp
+{
+    class ITHelpDeskBootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            
+           // return Container.Resolve<Shell>();
+             return new Shell();
+
+        }
+        protected override void InitializeShell()
+        {
+           // base.InitializeShell();
+
+            App.Current.MainWindow = (Window)this.Shell;
+            App.Current.MainWindow.Show();
+        }
+    }
+}
