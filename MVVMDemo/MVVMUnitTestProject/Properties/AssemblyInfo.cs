289edba4c513283a4c4669e85e97268aee7cc737
suki.xu@infor.com <suki.xu@infor.com>
2018-06-29 15:40:14 +0800

    add

---
+++ MVVMDemo/MVVMUnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MVVMUnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("infor.com")]
+[assembly: AssemblyProduct("MVVMUnitTestProject")]
+[assembly: AssemblyCopyright("Copyright © infor.com 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e634f163-abd6-4948-86f5-e2a7c2f387ff")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MVVMDemo/MVVMUnitTestProject/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MVVMDemo;
+using MVVMDemo.ViewModel;
+
+namespace MVVMUnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            StudentViewModel sViewModel = new StudentViewModel();
+            int count = sViewModel.GetStudentCount();
+            Assert.IsTrue(count == 3);
+        }
+    }
+}
