fd972f1a655798f3d6800d776039fa09791d61ab
Xian <lei.xian@siemens-healthineers.com>
2018-07-01 10:05:40 +0800

    add

---
+++ EffectiveCPart5Demo/ImplyISerializableDemo/MyDerivedType.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.Serialization;
+
+namespace ImplyISerializableDemo
+{
+    class MyDerivedType : MyType
+    {
+        private int myDerivedValue;
+
+        private MyDerivedType( SerializationInfo info, StreamingContext ct ) : base( info, ct )
+        {
+            myDerivedValue = info.GetInt32( "myDerivedValue" );
+        }
+
+        protected override void WriteObjectData( SerializationInfo info, StreamingContext context )
+        {
+            info.AddValue( "myDerivedValue", myDerivedValue );
+        }
+    }
+}
