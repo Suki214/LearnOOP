68e9f8e581befaf4a2e796b32b7eaba17cf5725d
Xian <lei.xian@siemens-healthineers.com>
2018-07-01 13:28:33 +0800

    modify

--- EffectiveCSharpPartTwo/EffectiveCSharpPart6Demo/EffectiveCSharpPart6Demo/Program.cs
+++ EffectiveCSharpPartTwo/EffectiveCSharpPart6Demo/EffectiveCSharpPart6Demo/Program.cs
@@ -10,12 +10,14 @@ namespace EffectiveCSharpPart6Demo
     {
         static void Main(string[] args)
         {
-            Customer c1= new Customer(){Revene = 1};
-            Customer c2= new Customer(){Revene = 2};
+            Customer c1= new Customer(){Revene = 3,Name = "C1"};
+            Customer c2= new Customer(){Revene = 2,Name = "C2"};
 
-            int result= c1.CompareTo( c2 );
+            int resultc= c1.CompareTo( c2 );
 
-            Console.WriteLine(result);
+            int resultr = c1.Revene.CompareTo( c2.Revene );
+
+            Console.WriteLine(resultc.ToString() +resultr.ToString());
             Console.ReadKey();
         }
     }
