a8eb539452726dbf689c3e9e8a4a40671b74365c
suki.xu@infor.com <suki.xu@infor.com>
2018-07-02 09:06:22 +0800

    add

--- EffectiveCPart5Demo/CallbackWithDelegate/Program.cs
+++ EffectiveCPart5Demo/CallbackWithDelegate/Program.cs
@@ -10,7 +10,22 @@ namespace CallbackWithDelegate
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("test for commite");
+            HelloDelegate sayHi = new HelloDelegate(SayEnglishHello);
+            sayHi += new HelloDelegate(SayChineseHello);
+            sayHi(" World!");
+            Console.ReadKey();
+        }
+
+        public static void SayEnglishHello(string english)
+        {
+            Console.WriteLine("Hi {0}",english);
+        }
+
+        public static void SayChineseHello(string chinese)
+        {
+            Console.WriteLine("你好 {0}", chinese);
         }
     }
+
+    public delegate void HelloDelegate(string hello);
 }
