8a9fcec89054a1b2d021435313cc41d02f66cf03
suki.xu@infor.com <suki.xu@infor.com>
2018-07-03 09:07:45 +0800

    add calculator for practice on code

---
+++ CalculatorMachine/CalculatorMachine/Model/Elements.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorMachine.Model
+{
+    public class Elements
+    {
+        public enum Operators
+        {
+            Plus,
+            Delete,
+            Multiply,
+            Devide,
+            EqualsTo
+        }
+    }
+}
---
+++ CalculatorMachine/CalculatorMachine/ViewModel/Calculate.cs
@@ -0,0 +1,55 @@
+﻿using CalculatorMachine.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CalculatorMachine.ViewModel
+{
+    class Calculate:INotifyPropertyChanged
+    {
+        private double inputValue;
+        private double leftValue;
+        private double rightValue;
+        private Elements.Operators operate;
+        public ICommand NumberClicked;
+
+        public Calculate(double leftValue, double rightValue, Elements.Operators operate)
+        {
+            this.leftValue = leftValue;
+            this.rightValue = rightValue;
+            this.operate = operate;
+        }
+
+        public double InputValue
+        {
+            get
+            {
+                return inputValue;
+            }
+            set
+            {
+                if (value != inputValue)
+                {
+                    inputValue = value;
+                    OnPropertyChanged("InputValue");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+
+    }
+}
