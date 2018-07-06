7e5895e0c6a0f9917a93c6d6dd752e45ec3761fb
suki.xu@infor.com <suki.xu@infor.com>
2018-07-06 09:19:14 +0800

    add

---
+++ StrategyPatternWithSpringDemo/StrategyPatternWithSpringDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using Spring.Context;
+using Spring.Context.Support;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPatternWithSpringDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IApplicationContext ctx = ContextRegistry.GetContext();
+            IUserInfoDal efDal = ctx.GetObject("UserInfoDal") as IUserInfoDal;
+            efDal.Show();
+            Console.ReadKey();
+        }
+    }
+}
