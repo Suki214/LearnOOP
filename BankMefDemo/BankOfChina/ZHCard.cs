92779e03c8c9719a4f71df686f449ed73e46f18b
Xian <lei.xian@siemens-healthineers.com>
2018-07-08 13:58:25 +0800

    add mef real case

---
+++ BankMefDemo/BankOfChina/ZHCard.cs
@@ -0,0 +1,31 @@
+﻿using BanksInterface;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankOfChina
+{
+    [Export(typeof(ICard))]
+    public class ZHCard : ICard
+    {
+        public double Money { get; set; }
+
+        public void CheckoutMoney(double money)
+        {
+            Money = Money - money;
+        }
+
+        public string GetAccountInfo()
+        {
+            return "Bank of China";
+        }
+
+        public void SaveMoney(double money)
+        {
+            Money = Money - money;
+        }
+    }
+}
