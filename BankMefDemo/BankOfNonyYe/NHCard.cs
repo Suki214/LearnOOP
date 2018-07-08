bd039416dbe18891f5f2df642fb7c2a6cd43bf8c
Xian <lei.xian@siemens-healthineers.com>
2018-07-08 14:10:01 +0800

    add mefbankdemo back

---
+++ BankMefDemo/BankOfNonyYe/NHCard.cs
@@ -0,0 +1,31 @@
+﻿using BanksInterface;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankOfNonyYe
+{
+    [Export(typeof(ICard))]
+    public class NHCard : ICard
+    {
+        public double Money { get; set; }
+
+        public void CheckoutMoney(double money)
+        {
+            Money -= money;
+        }
+
+        public string GetAccountInfo()
+        {
+            return "Nony Ye Bank";
+        }
+
+        public void SaveMoney(double money)
+        {
+            Money += money;
+        }
+    }
+}
---
+++ BankMefDemo/BanksInterface/ICard.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BanksInterface
+{
+    public interface ICard
+    {
+        double Money { get; set; }
+
+        string GetAccountInfo();
+
+        void SaveMoney(double money);
+
+        void CheckoutMoney(double money);
+    }
+}
