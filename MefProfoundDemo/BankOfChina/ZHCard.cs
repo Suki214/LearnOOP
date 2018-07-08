5801644f8d387340cfe8bde613731cd653a2ac70
Xian <lei.xian@siemens-healthineers.com>
2018-07-08 15:02:47 +0800

    add addition attribute calss for profound on MEF

---
+++ MefProfoundDemo/BankOfChina/ZHCard.cs
@@ -0,0 +1,31 @@
+﻿using BankInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankOfChina
+{
+    [ExportCard(CardType = "BankOFChina")]
+    public class ZHCard : ICard
+    {
+        public double Money { get ; set ; }
+       
+
+        public string GetCardInfo()
+        {
+            return "Bank Of China";
+        }
+
+        public void CheckoutMoney(double money)
+        {
+            Money -= money;
+        }
+
+        public void SaveMoney(double money)
+        {
+            Money += money;
+        }
+    }
+}
