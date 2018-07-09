1a22cf4875866055046929af07e4b824d040d79c
suki.xu@infor.com <suki.xu@infor.com>
2018-07-09 16:49:14 +0800

    add another simple prism demo

---
+++ WpfPrismDemo/WpfPrismDemo/WpfPrismDemoBootstrapper.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.UnityExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfPrismDemoModule;
+
+namespace WpfPrismDemo
+{
+    class WpfPrismDemoBootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return this.Container.TryResolve<Shell>();
+        }
+
+        protected override void InitializeModules()
+        {
+            base.InitializeModules();
+            Application.Current.MainWindow = (Window)this.Shell;
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureModuleCatalog()
+        {
+            base.ConfigureModuleCatalog();
+            ModuleCatalog moduleCatalog = (ModuleCatalog)this.ModuleCatalog;
+            moduleCatalog.AddModule(typeof(WpfPrismDemoModule.WpfPrismDemoModule));
+        }
+    }
+}
---
+++ WpfPrismDemo/WpfPrismDemoModule/WpfPrismDemoModule.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfPrismDemoModule
+{
+    public class WpfPrismDemoModule : IModule
+    {
+        private readonly IRegionViewRegistry regionViewRegistry;
+
+        public WpfPrismDemoModule(IRegionViewRegistry registry)
+        {
+            regionViewRegistry = registry;
+        }
+        public void Initialize()
+        {
+            regionViewRegistry.RegisterViewWithRegion("MainRegion", typeof(Views.WpfPrismDemoView));
+        }
+    }
+}
