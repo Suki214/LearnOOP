0555627bd0b5384a65af73016ba8c3ab7e1eddde
suki.xu@infor.com <suki.xu@infor.com>
2018-07-09 16:12:19 +0800

    modify

--- GetStartedPrismWPF/GetStartedPrismWPF.MainModule/GetStartedPrismWPFMainModule.cs
+++ GetStartedPrismWPF/GetStartedPrismWPF.MainModule/GetStartedPrismWPFMainModule.cs
@@ -1,10 +1,5 @@
 ﻿using Microsoft.Practices.Prism.Modularity;
 using Microsoft.Practices.Prism.Regions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GetStartedPrismWPF.MainModule
 {
--- GetStartedPrismWPF/GetStartedPrismWPF/PrismGetStartedBootstrapper.cs
+++ GetStartedPrismWPF/GetStartedPrismWPF/PrismGetStartedBootstrapper.cs
@@ -1,10 +1,5 @@
 ﻿using Microsoft.Practices.Prism.Modularity;
 using Microsoft.Practices.Prism.UnityExtensions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace GetStartedPrismWPF
