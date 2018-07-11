778824abed02be0147daa3e363cd2d4fa648fee7
suki.xu@infor.com <suki.xu@infor.com>
2018-07-11 16:03:00 +0800

    add wcf about calculator demo

---
+++ CalculatorWithWcfDemo/CalculatorWithWcfDemo/Service1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace CalculatorWithWcfDemo
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Service1 : IService1
+    {
+        public int Divide(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public int Sum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
---
+++ WCFDemo/SaleProject/SaleProject/Help.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true" CodeBehind="Help.aspx.cs" Inherits="SaleProject.Help" %>
+
+<asp:Content ID="HeaderContent" runat="server" ContentPlaceHolderID="HeadContent">
+</asp:Content>
+<asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
+    <h2>
+        Help
+    </h2>
+    <p>
+        Put content here.
+    </p>
+</asp:Content>
