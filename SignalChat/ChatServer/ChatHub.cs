6517f127d3874c5e97c57ea6a9d062de6cc4cd47
suki.xu@infor.com <suki.xu@infor.com>
2018-07-13 14:45:18 +0800

    add signal chat demo

---
+++ SignalChat/ChatServer/ChatHub.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNet.SignalR;
+using System.Collections.Concurrent;
+
+namespace ChatServer
+{
+    public class ChatHub:Hub<IClient>
+    {
+        private static ConcurrentDictionary<string, User> ChatClients = new ConcurrentDictionary<string, User>();
+    }
+}
---
+++ SignalChat/ChatServer/IClient.cs
@@ -0,0 +1,15 @@
+﻿namespace ChatServer
+{
+    public interface IClient
+    {
+        void PaticipantDisconnection(string name);
+        void PaticipantReconnection(string name);
+        void PaticipantLogin(User client);
+        void PaticipantLogout(string name);
+        void BroadcastTexMessage(string sender, string message);
+        void BroadcastPictureMessage(string sender, byte[] img);
+        void UnitcastTexMessage(string sender, string message);
+        void UnitcastPictureMessage(string sender, byte[] img);
+        void PaticipantTyping(string sender);
+    }
+}
