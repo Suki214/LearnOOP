69e24d02f1d2f5f49e622e0d3b46dfdc6d3529cd
suki.xu@infor.com <suki.xu@infor.com>
2018-07-18 16:50:34 +0800

    update

---
+++ SignalChat/ChatClient/Commands/RelayCommand.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ChatClient.Commands
+{
+    public class RelayCommand : ICommand
+    {
+        public readonly Action<object> _execute;
+        public readonly Predicate<object> _canExecute;
+
+        public RelayCommand(Action<object> execute) : this(execute, null) { }
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            _execute = execute ?? throw new ArgumentNullException("execute");
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute==null?true :_canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+    }
+
+    public class RelayCommand<T> : ICommand
+    {
+        public readonly Action<T> _execute;
+        public readonly Predicate<T> _canExecute;
+
+        public RelayCommand(Action<T> execute) : this(execute, null) { }
+
+        public RelayCommand(Action<T> execute, Predicate<T> canExecute)
+        {
+            _execute = execute ?? throw new ArgumentNullException("execute");
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute==null ? true : _canExecute((T)parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute((T)parameter);
+        }
+    }
+}
---
+++ SignalChat/ChatClient/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,60 @@
+﻿using ChatClient.Commands;
+using ChatClient.Services;
+using System.Windows.Input;
+using System.Drawing;
+
+namespace ChatClient.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private IDialogService dialogService;
+        private const int MAX_IMAGE_WIDTH = 150;
+        private const int MAX_IMAGE_HEIGHT = 150;
+        private string profilePic;
+
+        public string ProfilePic
+        {
+            get
+            {
+                return profilePic;
+            }
+            set
+            {
+                profilePic = value;
+                OnPropertyChanged("ProfilePic");
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+        }
+
+        private ICommand _SelectProfilePicCommand;
+        public ICommand SelectProfilePicCommand
+        {
+            get
+            {
+                if (_SelectProfilePicCommand == null)
+                {
+                    _SelectProfilePicCommand= new RelayCommand((o)=>SelectProfilePic());
+                }
+                return _SelectProfilePicCommand;
+            }
+        }
+
+        private void SelectProfilePic()
+        {
+            var pic = dialogService.OpenFile("Slect image file", "Images (*.jpg;*.png)|*.jpg;*.png");
+            if (!string.IsNullOrEmpty(pic))
+            {
+                var img = Image.FromFile(pic);
+                if(img.Width>MAX_IMAGE_WIDTH||img.Height > MAX_IMAGE_HEIGHT)
+                {
+                    dialogService.ShowNotification($"Image size should be {MAX_IMAGE_WIDTH} and {MAX_IMAGE_HEIGHT} or less");
+                    return;
+                }
+                ProfilePic = pic;
+            }
+        }
+    }
+}
