1d90e49b2576062eb763a8604512d5c37ded9a68
suki.xu@infor.com <suki.xu@infor.com>
2018-08-01 14:42:41 +0800

    add communication bettween usercontrol, unfinished

---
+++ CommunicationBetweenUserControls/CommunicationBetweenUserControls/RelayCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace CommunicationBetweenUserControls
+{
+    public class RelayCommand : ICommand
+    {
+        private Action<object> myExecute;
+        private Predicate<object> myCanExecute;
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            myCanExecute = canExecute;
+            myExecute = execute;
+        }
+        public RelayCommand(Action<object> execute) : this(execute,null) { }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            myExecute(parameter);
+        }
+    }
+}
---
+++ CommunicationBetweenUserControls/CommunicationBetweenUserControls/UCViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace CommunicationBetweenUserControls
+{    
+    public class UCViewModel:INotifyPropertyChanged
+    {
+        public event EventHandler SendWordsEventHandler;
+
+        private ICommand sendWordsCommand;
+        public ICommand SendWordsCommand
+        {
+            get
+            {
+                if (sendWordsCommand == null)
+                    sendWordsCommand = new RelayCommand((o)=>SendWords());
+                return sendWordsCommand;
+            }
+        }
+
+        private void SendWords()
+        {
+            SendWordsEventHandler(this, new EventArgs());
+        }        
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string property)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+
+        private string inputValue;
+        public string InputValue
+        {
+            get
+            {
+                return inputValue;
+            }
+            set
+            {
+                inputValue = value;
+                OnPropertyChanged("InputValue");
+            }
+        }
+    }
+}
