f1b9184eb8a2980b209f73c354672f95b2d8b4ca
suki.xu@infor.com <suki.xu@infor.com>
2018-08-07 15:20:41 +0800

    add generic demo with resolve DNS

---
+++ ResolveDNS/ResolveDNS/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ResolveDNS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s_host = "www.163.com";
+            
+            ResolveDNS resolver1 = new ResolveDNS();            
+            resolver1.Resolve(s_host);
+            int n = resolver1.IPLength;
+            Console.WriteLine("Get {1} IP Address of the host {0}",s_host,n);
+            Console.WriteLine();
+            for (int i = 0; i < n; i++)
+                Console.WriteLine(resolver1[i]);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ResolveDNS/ResolveDNS/ResolveDNS.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace ResolveDNS
+{
+    class ResolveDNS
+    {
+        IPAddress[] m_arrayIPs;
+
+        public void Resolve(string s_host)
+        {
+            IPHostEntry ip = Dns.GetHostByName(s_host);
+            m_arrayIPs = ip.AddressList;
+        }
+
+        public IPAddress this[int nIndex]
+        {
+            get
+            {
+                return m_arrayIPs[nIndex];
+            }
+        }
+
+        public int IPLength
+        {
+            get
+            {
+                return m_arrayIPs.Length;
+
+            }
+        }
+    }
+}
