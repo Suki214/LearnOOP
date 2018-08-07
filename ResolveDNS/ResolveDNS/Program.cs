421ce4cdeaf6d814344e5fedce179f2d67f94852
suki.xu@infor.com <suki.xu@infor.com>
2018-08-07 15:38:04 +0800

    update

--- ResolveDNS/ResolveDNS/Program.cs
+++ ResolveDNS/ResolveDNS/Program.cs
@@ -9,15 +9,48 @@ namespace ResolveDNS
     {
         static void Main(string[] args)
         {
-            string s_host = "www.163.com";
+            //string s_host = "www.163.com";
             
-            ResolveDNS resolver1 = new ResolveDNS();            
-            resolver1.Resolve(s_host);
-            int n = resolver1.IPLength;
-            Console.WriteLine("Get {1} IP Address of the host {0}",s_host,n);
-            Console.WriteLine();
-            for (int i = 0; i < n; i++)
-                Console.WriteLine(resolver1[i]);
+            //ResolveDNS resolver1 = new ResolveDNS();            
+            //resolver1.Resolve(s_host);
+            //int n = resolver1.IPLength;
+            //Console.WriteLine("Get {1} IP Address of the host {0}",s_host,n);
+            //Console.WriteLine();
+            //for (int i = 0; i < n; i++)
+            //    Console.WriteLine(resolver1[i]);
+
+
+
+             string host = "www.163.com";
+            ResolveDCN resolveDCN = new ResolveDCN();
+        resolveDCN.Resolve(host);
+            int m = resolveDCN.IpLength();
+            for (int i = 0; i < m; i++)
+            {
+                Console.WriteLine(resolveDCN[i]);
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
             Console.ReadLine();
         }
---
+++ ResolveDNS/ResolveDNS/ResolveDCN.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+
+namespace ResolveDNS
+{
+    internal class ResolveDCN
+    {
+        IPAddress[] iPAddress;
+        public IPAddress this[int ndex]
+        {
+            get
+            {
+                return iPAddress[ndex];
+            }
+        }
+        internal void Resolve(string host)
+        {
+            IPHostEntry iPHost = Dns.GetHostByName(host);
+            iPAddress = iPHost.AddressList;
+        }
+        internal int IpLength()
+        {
+            return iPAddress.Length;
+        }
+    }
+}
