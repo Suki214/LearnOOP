afa62c82648cd7e2d955cc3f135846758c46ab4b
suki.xu@infor.com <suki.xu@infor.com>
2018-08-07 16:47:43 +0800

    update with view creation demo

--- CalculatorWithMefDemo/HelloWorld/Controllers/TestController.cs
+++ CalculatorWithMefDemo/HelloWorld/Controllers/TestController.cs
@@ -6,12 +6,35 @@ using System.Web.Mvc;
 
 namespace HelloWorld.Controllers
 {
+    public class Customer
+    {
+        public string CustomerName { get; set; }
+        public string Address { get; set; }
+
+        public override string ToString()
+        {
+            return CustomerName + " in " + Address;
+        }
+    }
     public class TestController : Controller
     {
         // GET: Test
+        public Customer GetCustomer()
+        {
+            Customer c = new Customer();
+            c.Address = "Shanghai";
+            c.CustomerName = "CC";
+            return c;
+        }
+            
         public string GetString()
         {
             return "Hello World";
+        }
+
+        public ActionResult GetView()
+        {
+            return View("MyView");
         }
     }
 }
