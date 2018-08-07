f87564bbce43990cdf465a49005938a35b44742d
suki.xu@infor.com <suki.xu@infor.com>
2018-08-07 14:37:30 +0800

    Add demo with dynamic VS var

---
+++ DynamicSample/DynamicSample/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //In this case ,build successed, but run with error: No overload for method 'Welcome' takes '0' arguments
+            //dynamic obj = new DynamicTest();
+            //obj.Welcome();
+
+            //Need to correct as following way
+            //dynamic obj = new DynamicTest();
+            //string name = "name";
+            //obj.Welcome(name);
+
+            //If define as following, build is error
+            //There is no argument given that corresponds to the required formal parameter 'name' of 'DynamicTest.Welcome(string)'
+            //var v1 = new DynamicTest();
+            //v1.Welcome();
+
+
+            //var v1 = new DynamicTest();
+            //string name = "Tom";
+            //v1.Welcome(name);
+            Console.ReadKey();
+        }
+    }
+
+    class DynamicTest
+    {
+        public void Welcome(string name)
+        {
+            Console.WriteLine("Hello {0},welcome to dynamic world.", name);
+        }
+    }
+}
