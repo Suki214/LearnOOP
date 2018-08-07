d1a8378d073d7592322fd478a0a3058154d70c64
suki.xu@infor.com <suki.xu@infor.com>
2018-08-07 13:26:33 +0800

    some code with thread exception

---
+++ CommandPatternDemo/CommandPatternDemo/DemoToImplementIdispose.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandPatternDemo
+{
+    class DemoToImplementIdispose : IDisposable
+    {
+        private bool disposed = false;
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+                return;
+
+            if (disposing)
+            {
+                //dispose managed resource
+            }
+
+            //dispose un-managed resource
+            disposed = true;
+        }
+
+        ~DemoToImplementIdispose()
+        {
+            Dispose(false);//必须false
+        }
+    }
+}
--- CommandPatternDemo/CommandPatternDemo/Program.cs
+++ CommandPatternDemo/CommandPatternDemo/Program.cs
@@ -2,20 +2,84 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CommandPatternDemo
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Receiver r = new Receiver();
-            Command c = new ConcreteCommand(r);
-            Invoke i = new Invoke(c);
-            i.ExecuteCommand();
-            Console.ReadKey();
+        //static void Main(string[] args)
+        //{
+        //    Receiver r = new Receiver();
+        //    Command c = new ConcreteCommand(r);
+        //    Invoke i = new Invoke(c);
+        //    i.ExecuteCommand();
+        //    Console.ReadKey();
+        //}
 
+        //public static void Main()
+        //{
+        //    try
+        //    {
+        //        new Thread(Go).Start();
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        // 永远执行不到这儿！
+        //        Console.WriteLine("Exception!");
+        //    }
+        //}
+
+        //private static void Go()
+        //{
+        //    throw null;
+        //}
+
+
+        public static void Main()
+        {
+            new Thread(Go).Start();
+            Console.ReadKey();
+        }
+
+        private static void Go()
+        {
+            try
+            {
+                throw null; // 该异常将会被捕获
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception!");
+                // 异常日志记录，或者通知其他线程出现异常了
+            }
+        }        
+    }
+
+    public static class ThreadExecutor
+    {
+        public static bool Execute(System.Threading.WaitCallback callback, object state)
+        {
+            try
+            {
+                return System.Threading.ThreadPool.QueueUserWorkItem((data) =>
+                {
+                    try
+                    {
+                        callback(data);
+                    }
+                    catch (Exception ex)
+                    {
+                        // log the exception
+                    }
+                }, state);
+            }
+            catch (Exception e)
+            {
+                // log the exception
+            }
+            return false;
         }
     }
 }
