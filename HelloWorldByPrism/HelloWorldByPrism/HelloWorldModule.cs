96ceb913222c0b314a16c96474e7c4a41781b9da
suki.xu@infor.com <suki.xu@infor.com>
2018-08-13 16:52:45 +0800

    add demo about prism

---
+++ HelloWorldByPrism/HelloWorldByPrism/HelloWorldModule.cs
@@ -0,0 +1,105 @@
+﻿using HelloWorldByPrism.Views;
+using Microsoft.Practices.Unity;
+using Prism.Modularity;
+using Prism.Regions;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace HelloWorldByPrism
+{
+    public class HelloWorldModule : IModule
+    {
+        private readonly IRegionManager regionManager;
+        public IUnityContainer container { get; set; }
+        public HelloWorldModule(IRegionManager regionManager, IUnityContainer container)
+        {
+            this.regionManager = regionManager;
+            this.container = container;
+        }
+        public void Initialize()
+        {
+            RenderHelloWorldView();
+        }
+        private void RenderHelloWorldView()
+        {
+            IRegion mainRegion = this.regionManager.Regions["MainRegion"];
+            mainRegion.Add(new HelloWorldView());
+        }
+
+        private void RenderHelloWorldViewAgain()
+        {
+            IRegion mainRegion = regionManager.Regions["MainRegion"];
+            HelloWorldViewAgain viewAgain = container.Resolve<HelloWorldViewAgain>();
+            mainRegion.Add(viewAgain, "MainRegion");
+            viewAgain.DisplayHelloWorldView();
+        }
+
+        protected virtual RegionAdapterMappings ConfigureRegionAdapterMappings()
+        {
+            RegionAdapterMappings regionAdapterMappings = container.Resolve<RegionAdapterMappings>();
+            if (regionAdapterMappings != null)
+            {
+                regionAdapterMappings.RegisterMapping(typeof(Selector), this.container.Resolve<SelectorRegionAdapter>());
+                regionAdapterMappings.RegisterMapping(typeof(ItemsControl), this.container.Resolve<ItemsControlRegionAdapter>());
+                regionAdapterMappings.RegisterMapping(typeof(ContentControl), container.Resolve<ContentControlRegionAdapter>());
+            }
+            return regionAdapterMappings;
+        }    
+    }
+
+    public class StakPanelRegionAdapter : IRegionAdapter
+    {
+        public IRegion Initialize(object regionTarget, string regionName)
+        {
+            //StackPanel stackPanel = regionTarget as StackPanel;
+            //AllActiveRegion region = new AllActiveRegion();
+            //region.Name = regionName;
+            //region.ActiveViews.CollectionChanged += delegate
+            //{
+            //    stackPanel.Children.Clear();
+            //    foreach (var item in region.ActiveViews)
+            //    {
+            //        stackPanel.Children.Add(item as UIElement);
+            //    }
+            //};
+            if (regionName == null)
+                throw new ArgumentNullException("regionName");
+            IRegion region = CreateRegion();
+            region.Name = regionName;
+
+            SetObservableRegionOnHostingControl(region, regionTarget);
+            //Adapt(region, regionTarget);
+            return region;
+        }
+
+        private void SetObservableRegionOnHostingControl(IRegion region, object regionTarget)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+    public class StackPanelRegionAdapter : RegionAdapterBase<StackPanel>
+    {
+        public StackPanelRegionAdapter(IRegionBehaviorFactory defauleBehaviors) : base(defauleBehaviors) { }
+
+        protected override void Adapt(IRegion region, StackPanel regionTarget)
+        {
+            region.ActiveViews.CollectionChanged += delegate
+            {
+                regionTarget.Children.Clear();
+                foreach (var item in region.ActiveViews)
+                {
+                    regionTarget.Children.Add(item as UIElement);
+                }
+            };
+        }
+
+        protected override IRegion CreateRegion()
+        {
+            return new AllActiveRegion();
+        }
+    }
+}
---
+++ HelloWorldByPrism/HelloWorldByPrism/Views/HelloWorldViewAgain.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelloWorldByPrism.Views
+{
+    /// <summary>
+    /// Interaction logic for HelloWorldViewAgain.xaml
+    /// </summary>
+    public partial class HelloWorldViewAgain : UserControl
+    {
+        private readonly IRegionManager regionManager;
+        public HelloWorldViewAgain()
+        {
+            InitializeComponent();
+        }
+
+        public void DisplayHelloWorldView()
+        {
+            var view = this.regionManager.Regions["MainRegion"].GetView("HelloWorldViewAgain") as DependencyObject;
+            IRegion region = RegionManager.GetRegionManager(view).Regions["HelloWorldViewAgain"];
+            region.Add(new HelloWorldView(), "Hello");
+        }
+
+
+    }
+}
