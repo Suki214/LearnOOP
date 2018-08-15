2b011c1d81a19c61f0fb566c1f4b1ba6804173fb
suki.xu@infor.com <suki.xu@infor.com>
2018-08-15 15:42:31 +0800

    add serializable demo

---
+++ SerializableDemo/SerializableDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Formatters.Soap;
+using System.Xml.Serialization;
+
+namespace SerializableDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var me = new Student() { StudentId = 1, Name = "SX", Age = 18 };
+            IFormatter formatter = new BinaryFormatter();
+            IFormatter formatter1 = new SoapFormatter();
+            XmlSerializer formatter2 = new XmlSerializer(typeof(Student));
+            Stream stream = new FileStream("C:/data/studentInfo.txt",FileMode.OpenOrCreate,FileAccess.ReadWrite,FileShare.None);
+            formatter.Serialize(stream,me);
+            stream.Close();
+
+            Stream destream = new FileStream("C:/data/studentInfo.txt", FileMode.Open, FileAccess.Read, FileShare.Read);
+            var stillme= (Student)formatter.Deserialize(destream);
+            destream.Close();
+            Console.WriteLine( stillme.DisplayInfo());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SerializableDemo/SerializableDemo/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializableDemo
+{
+    [Serializable]
+    class Student
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string DisplayInfo()
+        {
+             return "Id is "+ StudentId.ToString()+ "Name is "+ Name + "Age is "+Age.ToString();
+        }
+    }
+}
