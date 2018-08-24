6743dc6b14c333dc2571cf06e5872b2fcb43a36d
suki.xu@infor.com <suki.xu@infor.com>
2018-08-24 15:04:59 +0800

    add test about protected data scope among projects

--- ScoreSystem/ScoreSystem/Score.cs
+++ ScoreSystem/ScoreSystem/Score.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TestProject;
 
 namespace ScoreSystem
 {
-   internal class Score
+   internal class Score:ProtectedClass
     {
         public string StudentName { get; set; }
         public int MathScore { get; set; }
         public string ID { get; set; }
+
+        public void methods()
+        {
+            method();
+        }
     }
 }
