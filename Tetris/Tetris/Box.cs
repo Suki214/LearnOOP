6b6ae6c0bd61fd47c3beefd389afbe8548d758c9
suki.xu@infor.com <suki.xu@infor.com>
2018-09-21 13:52:14 +0800

    update

--- Tetris/Tetris/Box.cs
+++ Tetris/Tetris/Box.cs
@@ -8,8 +8,15 @@ namespace Tetris
 {
     abstract class Box
     {
+        /// <summary>
+        /// 定义四个方格组成图形
+        /// </summary>
         protected IList<Rectangle> rectangles = new List<Rectangle>(4);
+
         DispatcherTimer dispatcherTimer;
+
+        public bool IsPause { get; private set; }
+
         public Box()
         {
             for(int i = 0; i < 4; i++)
@@ -26,7 +33,65 @@ namespace Tetris
 
         private void Timer_Tick(object sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            if (!MoveDown())
+            {
+                dispatcherTimer.Stop();
+                OnButtom(this, null);
+            }
+        }
+
+        public event EventHandler OnButtom;
+
+        private bool MoveDown()
+        {
+            return Move(0, 1);
+        }
+
+        protected bool Move(int _x, int _y)
+        {
+            if (IsPause) return false;
+            for(int i = 0; i < 4; i++)
+            {
+                int x = Convert.ToInt32(rectangles[i].GetValue(Grid.ColumnProperty)) + _x;
+                int y = Convert.ToInt32(rectangles[i].GetValue(Grid.RowProperty)) + _y;
+                if (Existence(x, y)) return false;
+                if (!InGrid(x, y)) return false;
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                rectangles[i].SetValue(Grid.ColumnProperty, Convert.ToInt32(rectangles[i].GetValue(Grid.ColumnProperty)) + _x);
+                rectangles[i].SetValue(Grid.RowProperty, Convert.ToInt32(rectangles[i].GetValue(Grid.RowProperty)) + _y);
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// 判断由数值 x, y标示的行列值是否在Grid范围内
+        /// </summary>
+        protected bool InGrid(int x, int y)
+        {
+            if (x >= 13 || y >= 20 || x < 0 || y < 0) return false;
+            return true;
+        }
+
+        protected Grid grid;
+        /// <summary>
+        /// 判断x行y列是否包含方格
+        /// </summary>
+        protected bool Existence(int x, int y)
+        {
+            foreach(var r in grid.Children)
+            {
+                if(r is Rectangle)
+                {
+                    if (rectangles.Contains(r as Rectangle)) return false;
+                    if (Convert.ToInt32((r as Rectangle).GetValue(Grid.ColumnProperty)) == x && Convert.ToInt32((r as Rectangle).GetValue(Grid.RowProperty)) == y)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
         }
     }
 }
